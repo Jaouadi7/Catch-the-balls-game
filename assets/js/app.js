46dbe9e9f4918cd0c7a0df90e47510b66bf84b66
Mohamed A. Eljaouadi <39972749+Jaouadi7@users.noreply.github.com>
2021-10-30 22:19:54 +0100

    fix game background images

--- assets/js/app.js
+++ assets/js/app.js
@@ -148,7 +148,9 @@ class Game {
     let imgId = 1; // START SHOW IMAGE 1 TO 5
 
     // SET BACKGROUND CSS PROPS
-    defaultBgContainer.style.backgroundImage = `url('../javascript-catch-balls-game/assets/imgs/backgrounds/underwater-background-${imgId}.jpg')`;
+    url('../imgs/backgrounds/underwater-background-1.jpg') no-repeat
+    bottom center fixed
+    defaultBgContainer.style.backgroundImage = `url('../imgs/backgrounds/underwater-background-${imgId}.jpg')`;
     defaultBgContainer.style.backgroundRepeat = 'no-repeat';
     defaultBgContainer.style.backgroundPosition = 'bottom center';
     defaultBgContainer.style.backgroundSize = 'cover';
@@ -160,12 +162,12 @@ class Game {
 
       if ( imgId <= 5 ) {
 
-        defaultBgContainer.style.backgroundImage = `url('../javascript-catch-balls-game/assets/imgs/backgrounds/underwater-background-${imgId}.jpg')`;
+        defaultBgContainer.style.backgroundImage = `url('../imgs/backgrounds/underwater-background-${imgId}.jpg')`;
 
       } else {
 
         imgId = 1;
-        defaultBgContainer.style.backgroundImage = `url('../javascript-catch-balls-game/assets/imgs/backgrounds/underwater-background-${imgId}.jpg')`;
+        defaultBgContainer.style.backgroundImage = `url('../imgs/backgrounds/underwater-background-${imgId}.jpg')`;
 
       }
     });
